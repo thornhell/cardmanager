fe59bfbd2b8ab2118c85a43d09acf91b45462d8b
thornhell <mariuszsak@gmail.com>
2020-08-29 19:03:51 +0200

    Some fixes.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {AppStateProvider} from './AppContext';
+import {AppStateProvider} from './context/AppContext';
 
 ReactDOM.render(
     <React.StrictMode>
