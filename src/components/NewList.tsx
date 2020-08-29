5f8d4f7d8058b02704b2d501639fd7a83612eaae
thornhell <mariuszsak@gmail.com>
2020-08-29 16:07:29 +0200

    Component with style added.

---
+++ src/components/NewList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import'./newList.css';
+
+const NewList = () => {
+    return (
+        <div>
+            <button className='newList'>+ add new list</button>
+        </div>
+    );
+};
+
+export default NewList;
