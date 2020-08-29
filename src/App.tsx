a8571fc63899045fe0855380f3945a7fbe0518cb
thornhell <mariuszsak@gmail.com>
2020-08-29 19:02:21 +0200

    Changes due to switch to context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Column from './components/Column';
-import Card from './components/Card';
 import './app.css'
 import NewList from './components/NewList';
+import {useAppState} from './context/AppContext'
 
-function App() {
+const App = () => {
+    const {state} = useAppState();
     return (
         <div className='app'>
-            <Column text='Ideas'><Card text='Card1'/></Column>
-            <Column text='...in progress... '><Card text='Card2'/></Column>
-            <Column text='Finished'><Card text='Card3'/></Column>
+            {state.lists.map((list, index) =>
+                <Column text={list.text} key={list.id} index={index}/>
+            )}
             <NewList/>
         </div>
     );
