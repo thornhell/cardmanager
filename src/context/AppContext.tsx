e160b84b7881b4b5748d052ff15710372d276a12
thornhell <mariuszsak@gmail.com>
2020-08-29 22:56:43 +0200

    Changes due to state management.

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,15 +1,18 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, useContext, useReducer} from 'react';
 import appLists, { AppState } from './AppLists';
+import AppStateReducer from './AppStateReducer';
 
 interface AppStateContextProps {
-    state: AppState
+    state: AppState;
+    dispatch: any
 }
 
 const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps);
 
 export const AppStateProvider = ({children}: React.PropsWithChildren<{}>) => {
+    const [state, dispatch] = useReducer(AppStateReducer, appLists)
     return (
-        <AppStateContext.Provider value={{state: appLists}}>
+        <AppStateContext.Provider value={{state, dispatch}}>
             {children}
         </AppStateContext.Provider>
     )
--- src/context/AppStateReducer.tsx
+++ src/context/AppStateReducer.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import {AppState} from './AppLists';
 import {AddNewList, AddNewTask} from './Actions';
+import {v4 as uuidv4 } from 'uuid';
+import { findItemIndexById } from '../utils/finditemById';
 
 const AppStateReducer = (state: AppState, action: AddNewList | AddNewTask): AppState => {
     switch (action.type) {
@@ -8,11 +9,19 @@ const AppStateReducer = (state: AppState, action: AddNewList | AddNewTask): AppS
             return {
                 ...state,
                 lists: [
-                    ...state.lists, {id: uuid(), text: action.payload, tasks: []}
+                    ...state.lists, {id: uuidv4(), text: action.payload, tasks: []}
                 ]
             }
         }
         case 'ADD_TASK': {
+            const targetLaneIndex = findItemIndexById(
+                state.lists,
+                action.payload.id
+            )
+            state.lists[targetLaneIndex].tasks.push({
+                id: uuidv4(),
+                text: action.payload.text
+            })
             return {
                 ...state
             }
