77fabf0dc3575a8b4027da4212ddb8489be2de39
thornhell <mariuszsak@gmail.com>
2020-08-29 16:09:41 +0200

    Some changes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Column from './components/Column';
 import Card from './components/Card';
 import './app.css'
+import NewList from './components/NewList';
 
 function App() {
-  return (
-    <div className='app'>
-      <Column><Card /></Column>
-      <Column><Card /></Column>
-      <Column><Card /></Column>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <Column text='Ideas'><Card text='Card1'/></Column>
+            <Column text='...in progress... '><Card text='Card2'/></Column>
+            <Column text='Finished'><Card text='Card3'/></Column>
+            <NewList/>
+        </div>
+    );
 }
 
 export default App;
