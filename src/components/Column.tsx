fa126302e49b7273472dd59b6dbf822595941232
thornhell <mariuszsak@gmail.com>
2020-08-29 19:03:04 +0200

    Changes due to switch to context.

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './column.css'
+import {useAppState} from '../context/AppContext';
+import Card from './Card';
 
 interface ColumnProps {
     text: string;
+    index: number;
 }
 
-const Column = ({text, children}:React.PropsWithChildren<ColumnProps>) => {
+const Column = ({text, index}: ColumnProps) => {
+    const {state} = useAppState();
     return (
         <div className='column'>
             <h3>{text}</h3>
-            {children}
+            {state.lists[index].tasks.map(task =>
+                <Card text={task.text} key={task.id}/>
+            )}
             + add new item
         </div>
     );
