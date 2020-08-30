269e76e0161dc54472b099f7ad4842a9f1983578
thornhell <mariuszsak@gmail.com>
2020-08-30 14:47:55 +0200

    No item/list added if payload is empty.

--- src/context/AppStateReducer.tsx
+++ src/context/AppStateReducer.tsx
@@ -1,19 +1,25 @@
 import {AppState} from './AppLists';
 import {AddNewList, AddNewTask} from './Actions';
-import {v4 as uuidv4 } from 'uuid';
-import { findItemIndexById } from '../utils/finditemById';
+import {v4 as uuidv4} from 'uuid';
+import {findItemIndexById} from '../utils/finditemById';
 
 const AppStateReducer = (state: AppState, action: AddNewList | AddNewTask): AppState => {
     switch (action.type) {
-        case 'ADD_LIST': {
+        case 'ADD_LIST':
+            if (!action.payload) {
+                return state;
+            }
             return {
                 ...state,
                 lists: [
                     ...state.lists, {id: uuidv4(), text: action.payload, tasks: []}
                 ]
             }
-        }
+
         case 'ADD_TASK': {
+            if (!action.payload.text) {
+                return state;
+            }
             const targetLaneIndex = findItemIndexById(
                 state.lists,
                 action.payload.id
