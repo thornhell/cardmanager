89a8f35365ae2995a863a34e05436cc6e7cc8e50
thornhell <mariuszsak@gmail.com>
2020-08-30 18:27:53 +0200

    Added styles.

--- src/styles.ts
+++ src/styles.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 interface DragPreviewContainerProps {
-    isHidden?: boolean
+    isHidden?: boolean;
+    isPreview?: boolean;
 }
 
 export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
-opacity: ${props => (props.isHidden ? 0.3 : 1)};
+transform: ${props => (props.isPreview ? "rotate(5deg)" : undefined)};
+opacity: ${props => (props.isHidden ? 0 : 1)};
 `
 
 export const ColumnContainer = styled(DragPreviewContainer)`   
@@ -16,4 +18,14 @@ margin-right: 20px;
 border-radius: 3px;
 padding: 8px 8px;
 flex-grow: 0;
+`
+
+export const CustomDragLayerContainer = styled.div`
+height: 100%;
+left: 0;
+pointer-events: none;
+position: fixed;
+top: 0;
+width: 100%;
+z--index: 100;
 `
