0380432a7226ad6c7880f87aa0c9cb3f7d519fa3
thornhell <mariuszsak@gmail.com>
2020-08-30 14:57:31 +0200

    New type added.

--- src/context/Actions.tsx
+++ src/context/Actions.tsx
@@ -1,5 +1,6 @@
 export const ADD_LIST = 'ADD_LIST';
 export const ADD_TASK = 'ADD_TASK';
+export const MOVE_LIST = 'MOVE_LIST';
 
 export interface AddNewList {
     type: typeof ADD_LIST;
@@ -11,5 +12,13 @@ export interface AddNewTask {
     payload: {
         text: string,
         id: string
+    }
+}
+
+export interface MoveList {
+    type: typeof MOVE_LIST;
+    payload: {
+        dragIndex: number,
+        hoverIndex: number
     }
 }
