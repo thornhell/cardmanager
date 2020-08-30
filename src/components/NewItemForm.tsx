054dfaeefd7245ba7d9979a7a7a289d1db8d6ddd
thornhell <mariuszsak@gmail.com>
2020-08-30 13:03:10 +0200

    Added some css.

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {useFocus} from '../utils/useFocus'
+import './newItemForm.css'
+import './newList.css'
 
 interface NewItemFormProps {
     onAdd(text: string): void
@@ -10,13 +12,16 @@ const NewItemForm = ({onAdd}: NewItemFormProps) => {
     const inputRef = useFocus()
 
     return (
-        <div>
+        <div className='container'>
             <input
+                className='inputForm'
                 ref={inputRef}
                 value={text}
                 onChange={e => setText(e.target.value)}
             />
-            <button onClick={() => onAdd(text)}>
+            <button
+                className='btn'
+                onClick={() => onAdd(text)}>
                 Create
             </button>
         </div>
