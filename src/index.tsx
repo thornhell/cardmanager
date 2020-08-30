467e0229ed8745022b1592a9b5cf16d9117dd240
thornhell <mariuszsak@gmail.com>
2020-08-30 15:10:15 +0200

    Dnd + backend added.

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {AppStateProvider} from './context/AppContext';
+import {DndProvider} from "react-dnd"
+import {HTML5Backend} from "react-dnd-html5-backend"
 
 ReactDOM.render(
     <React.StrictMode>
-        <AppStateProvider>
-            <App/>
-        </AppStateProvider>
+        <DndProvider backend={HTML5Backend}>
+            <AppStateProvider>
+                <App/>
+            </AppStateProvider>
+        </DndProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
