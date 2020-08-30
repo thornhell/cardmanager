1ba6f826303c58dfe363601e048d3143b60a699c
thornhell <mariuszsak@gmail.com>
2020-08-30 12:06:30 +0200

    Dispatcher added, some refactoring.

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,21 +2,31 @@ import React from 'react';
 import './column.css'
 import {useAppState} from '../context/AppContext';
 import Card from './Card';
+import AddNewItem from './AddNewItem';
 
 interface ColumnProps {
     text: string;
     index: number;
+    id: string;
 }
 
-const Column = ({text, index}: ColumnProps) => {
-    const {state} = useAppState();
+const Column = ({text, index, id}: ColumnProps) => {
+    const {state, dispatch} = useAppState();
+
     return (
         <div className='column'>
             <h3>{text}</h3>
             {state.lists[index].tasks.map(task =>
-                <Card text={task.text} key={task.id}/>
+                <Card
+                    text={task.text}
+                    key={task.id}
+                />
             )}
-            + add new item
+            <AddNewItem onAdd={text =>
+                dispatch({
+                    type: "ADD_TASK",
+                    payload: { text, id: id }
+                })} />
         </div>
     );
 };
